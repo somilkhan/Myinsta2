312b953942670ad951a3584cd7b9c02c9c905688
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-03 01:14:18 +0530

    feat: define Instagram 445 compatibility target

---
+++ patches/src/main/kotlin/dev/zehen/myinsta2/shared/Constants.kt
@@ -0,0 +1,20 @@
+package dev.zehen.myinsta2.shared
+
+import app.morphe.patcher.patch.ApkFileType
+import app.morphe.patcher.patch.AppTarget
+import app.morphe.patcher.patch.Compatibility
+
+object Constants {
+    val INSTAGRAM_445 = Compatibility(
+        name = "Instagram",
+        packageName = "com.instagram.android",
+        apkFileType = ApkFileType.APK,
+        appIconColor = 0xE1306C,
+        targets = listOf(
+            AppTarget(
+                version = "445.0.0.45.83",
+                versionCode = 385111379,
+            ),
+        ),
+    )
+}
