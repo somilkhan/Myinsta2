9c6b820db5f313603bf52fcccaa8f2a65b5ae9ab
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-03 01:42:25 +0530

    chore: keep root patch compatible with target

--- patches/src/main/kotlin/dev/zehen/myinsta2/MyInsta2.kt
+++ patches/src/main/kotlin/dev/zehen/myinsta2/MyInsta2.kt
@@ -2,7 +2,6 @@ package dev.zehen.myinsta2
 
 import app.morphe.patcher.patch.bytecodePatch
 
-/** Root patch for the MyInsta2 Instagram integration. */
 @Suppress("unused")
 val myInsta2Patch =
     bytecodePatch(
