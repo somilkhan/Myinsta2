8eb0765fd4ca7a6306b62ef2effae8a937b19079
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-06 14:52:23 +0530

    Expose enum values accessor in Instagram 445 extension stub

--- extensions/myinsta/stub/src/main/java/com/instagram/feed/media/mediaoption/MediaOption$Option.java
+++ extensions/myinsta/stub/src/main/java/com/instagram/feed/media/mediaoption/MediaOption$Option.java
@@ -3,4 +3,8 @@ package com.instagram.feed.media.mediaoption;
 /** Compile-only shape of Instagram 445's obfuscated MediaOption$Option type. */
 public class MediaOption$Option {
     public MediaOption$Option(String name, int index, int iconRes) {}
+
+    public static MediaOption$Option[] $values() {
+        return new MediaOption$Option[0];
+    }
 }
