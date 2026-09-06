01094125e8cf2f984d35a0dda5bcf9a228651db6
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-06 14:40:34 +0530

    Add MediaOption constructor stub

---
+++ extensions/myinsta/stub/src/main/java/com/instagram/feed/media/mediaoption/MediaOption$Option.java
@@ -0,0 +1,6 @@
+package com.instagram.feed.media.mediaoption;
+
+/** Compile-only shape of Instagram 445's obfuscated MediaOption$Option type. */
+public class MediaOption$Option {
+    public MediaOption$Option(String name, int index, int iconRes) {}
+}
