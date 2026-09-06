cbfcd907997ee8f41785b5c94836f62a5c6e4022
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-06 14:19:52 +0530

    Remove unimplemented 445 features from production bundle

--- patches/src/main/kotlin/dev/zehen/myinsta2/MyInsta2.kt
+++ patches/src/main/kotlin/dev/zehen/myinsta2/MyInsta2.kt
@@ -2,12 +2,10 @@ package dev.zehen.myinsta2
 
 import app.morphe.patcher.patch.bytecodePatch
 import dev.zehen.myinsta2.ads.hideAdsPatch
-import dev.zehen.myinsta2.download.downloadMediaPatch
 import dev.zehen.myinsta2.ghostmode.antiRevokePatch
 import dev.zehen.myinsta2.ghostmode.ghostModePatch
 import dev.zehen.myinsta2.ghostmode.ghostModeTypingPatch
 import dev.zehen.myinsta2.ghostmode.viewStoriesAnonymouslyPatch
-import dev.zehen.myinsta2.stories.disableStoryAutoFlipPatch
 import dev.zehen.myinsta2.stories.disableVideoAutoplayPatch
 
 /** MyInsta2 feature bundle for Instagram 445. */
@@ -18,13 +16,11 @@ val myInsta2Patch = bytecodePatch(
     default = true,
 ) {
     dependsOn(
-        downloadMediaPatch,
         ghostModePatch,
         ghostModeTypingPatch,
         viewStoriesAnonymouslyPatch,
         antiRevokePatch,
         hideAdsPatch,
-        disableStoryAutoFlipPatch,
         disableVideoAutoplayPatch,
     )
 }
