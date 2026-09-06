80cfcff24028a7588096427036b2604fac238561
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-07 01:23:23 +0530

    Add compile-only Kotlin Function0 shape

---
+++ extensions/myinsta/stub/src/main/java/kotlin/jvm/functions/Function0.java
@@ -0,0 +1,7 @@
+package kotlin.jvm.functions;
+
+/** Compile-only shape of Kotlin's Function0 interface for Instagram 445 stubs. */
+public interface Function0<R> {
+    R invoke();
+    int getArity();
+}
