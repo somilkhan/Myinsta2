2400f1c4006f97bd97e85f514824243cff745baf
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-07 04:38:42 +0530

    Enable validated 445 story auto-flip patch in MyInsta2

--- patches/src/main/kotlin/dev/zehen/myinsta2/MyInsta2.kt
+++ patches/src/main/kotlin/dev/zehen/myinsta2/MyInsta2.kt
@@ -6,6 +6,7 @@ import dev.zehen.myinsta2.ghostmode.antiRevokePatch
 import dev.zehen.myinsta2.ghostmode.ghostModePatch
 import dev.zehen.myinsta2.ghostmode.ghostModeTypingPatch
 import dev.zehen.myinsta2.ghostmode.viewStoriesAnonymouslyPatch
+import dev.zehen.myinsta2.stories.disableStoryAutoFlipPatch
 import dev.zehen.myinsta2.stories.disableVideoAutoplayPatch
 
 /** MyInsta2 production feature bundle for Instagram 445. */
@@ -22,5 +23,6 @@ val myInsta2Patch = bytecodePatch(
         antiRevokePatch,
         hideAdsPatch,
         disableVideoAutoplayPatch,
+        disableStoryAutoFlipPatch,
     )
 }
