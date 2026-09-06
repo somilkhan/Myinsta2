086c44192475d2936e583776e0225fa77c98ca72
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-07 01:17:40 +0530

    Add 445 comment button stub

---
+++ extensions/myinsta/stub/src/main/java/LX/LKA.java
@@ -0,0 +1,10 @@
+package LX;
+
+import kotlin.jvm.functions.Function0;
+
+/** Compile-only shape of Instagram 445's comment action button model. */
+public class LKA {
+    public static XZR A00;
+
+    public LKA(XZR style, LAb drawable, LAc label, Function0<?> action) {}
+}
